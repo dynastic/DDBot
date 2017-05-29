7ab5bc3f38486cd440b602dbb83eb41a85f06ce2
Eric Rabil <EricRabil@users.noreply.github.com>
2017-05-29 02:14:23 -0400

    Fixed broken play command

--- Source/Commands/Music/play.js
+++ Source/Commands/Music/play.js
@@ -3,15 +3,13 @@ const yt = require('ytdl-core');
 
 module.exports = new Command("play", "plays a song from youtube", "<url>", ["yt"],
     (client, message, response, args) => {
-        if(!message.member.voiceChannel) {
-            return response.reply("", response.embedFactory.createErrorEmbed(null, message.member).setDescription("You need to be in a voice channel."));
-            message.member.voiceChannel.join().then(connection => {
-                var stream = yt(args[0] || "https://www.youtube.com/watch?v=dQw4w9WgXcQ", {audioonly: true});
-                const dispatcher = connnection.playStream(stream);
-                dispatcher.on('end', () => {
-                    message.member.voiceChannel.leave();
-                });
+        if(!message.member.voiceChannel) return response.reply("", response.embedFactory.createErrorEmbed(null, message.member).setDescription("You need to be in a voice channel."));
+        message.member.voiceChannel.join().then(connection => {
+            var stream = yt(args[0] || "https://www.youtube.com/watch?v=dQw4w9WgXcQ", {audioonly: true});
+            const dispatcher = connnection.playStream(stream);
+            dispatcher.on('end', () => {
+                message.member.voiceChannel.leave();
             });
-        }
+        }).catch(e => client.log(e, true));
     }
 );
