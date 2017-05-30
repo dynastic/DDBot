3e8735a100db3cdba330bf62feb01c4afa056016
Eric Rabil <EricRabil@users.noreply.github.com>
2017-05-30 15:40:08 -0400

    Overhauled how guild configuration is stored

--- Source/Managers/GuildInstance.js
+++ Source/Managers/GuildInstance.js
@@ -5,13 +5,15 @@ class GuildInstance {
     constructor(guild) {
         this.guild = guild;
         this.client = guild.client;
-        this.properties = this.client.getGuildConfig(guild);
-        if(this.properties.disabledCommands.length == 0){
-            this.commandsManager = this.client.commandsManager;
-        } else {
-            this.commandsManager = new Commands(this.client, this.properties.disabledCommands);
-        }
-        this.messagesManager = new Messages(this.guild);
+        this.client.getGuildConfig(this.guild).then(prop => {
+            this.properties = prop;
+            if(this.properties.disabledCommands.length == 0){
+                this.commandsManager = this.client.commandsManager;
+            } else {
+                this.commandsManager = new Commands(this.client, this.properties.disabledCommands);
+            }
+            this.messagesManager = new Messages(this.guild);
+        });
     }
 }
 
