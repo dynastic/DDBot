53073fe8b5438b4e45bbdd031c0e8bbf74069d6a
Eric Rabil <EricRabil@users.noreply.github.com>
2017-05-30 23:14:43 -0400

    More safeguards

--- Source/Commands/Administration/delegate.js
+++ Source/Commands/Administration/delegate.js
@@ -7,13 +7,16 @@ module.exports = new Command("delegate", "Delegate guild management to a user", 
     (client, message, response, args) => {
         if(args.length != 2) return response.reply("", response.embedFactory.createErrorEmbed("Invalid Arguments", message.member).setDescription("Please specify exactly two arguments"));
         var guild = args[0];
+        if (guild == client.user.id) return response.reply("", response.embedFactory.createErrorEmbed().setDescription(`<@${client.user.id}> cannot be delegated to a guild.`))
         var user = args[1];
         var config = GuildConfig.findOne({snowflake: guild}, (err, docs) => {
             if (err) return response.reply("", response.embedFactory.createErrorEmbed().setDescription(`Something went wrong while delegating ownership:\n\`\`\`${err}\`\`\``));
             if (!docs) return response.reply("", response.embedFactory.createErrorEmbed().setDescription(`${message.client.config.botName} is not a member of that guild.`));
+            if (!client.guilds.get(guild)) return response.reply("", response.embedFactory.createErrorEmbed().setDescription(`${message.client.config.botName} is not a member of that guild.`));
+            if (!client.guilds.get(guild).members.get(user)) return response.reply("", response.embedFactory.createErrorEmbed().setDescription(`<@${user}> is not a member of that guild.`));
             docs.delegate = user;
             docs.save();
-            return response.reply("", response.embedFactory.createSuccessEmbed("Guild Ownership Delegated", message.member).setDescription(`Guild was successfully delegated to <@${message.author.id}>`));
+            return response.reply("", response.embedFactory.createSuccessEmbed("Guild Ownership Delegated", message.member).setDescription(`Guild was successfully delegated to <@${user}>`));
         })
     }, [], true, true
 );
