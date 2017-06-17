5721a2ef657fa7094af268b7384820105137c1dc
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-17 19:45:01 -0400

    EvalShortcut module :)

---
+++ Source/Modules/EvalShortcut/evalshortcut.js
@@ -0,0 +1,23 @@
+class EvalShortcut {
+    constructor(client) {
+        this.client = client;
+        this.response = this.client.resolve('response');
+        if(!this.response) return this.client.log("[EvalShortcut] Disabling because response class could not be found", true)
+        this.response = require(this.response);
+        this.meta = require('./module')
+        this.config = require('./config')
+        this.events = {
+            message: (message) => {
+                if(message.content == this.config.prefix || !message.content.startsWith(this.config.prefix) || message.content.startsWith(this.config.prefix + this.config.prefix)) return;
+                this.client.commandsManager.get('eval').then(c => {
+                    if(!c) return;
+                    var args = message.content.split(" ");
+                    args[0] = args[0].slice(this.config.prefix.length);
+                    c.execute(this.client, message, new response(message), args);
+                });
+            }
+        }
+    }
+}
+
+module.exports = EvalShortcut;
