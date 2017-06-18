e73d0c52fe51e65df42ecf6a99d810e386635b88
Eric Rabil <ericjrabil@gmail.com>
2017-06-18 15:15:04 -0400

    Revert "Added versioning system and a not-annoying update message"
    This reverts commit 5e3234272c4dfbb15b67c0be17a8fe41307dc0fd.

--- Source/client.js
+++ Source/client.js
@@ -3,7 +3,6 @@ const fs = require("fs-extra-promise");
 const path = require("path");
 const mongoose = require('mongoose');
 const autoIncrement = require("mongoose-auto-increment");
-const https = require('https');
 
 const ModuleManager = require("./Managers/Modules");
 const CommandsManager = require("./Managers/Commands");
@@ -52,22 +51,6 @@ const defaultGuildConfig = {
 const client = new class extends Discord.Client {
     constructor() {
         super();
-        this.meta = require("../package");
-        this.config = require("./config");
-        https.get('https://raw.githubusercontent.com/dynasticdevelop/DDBot/master/package.json', r => {
-            var body = '';
-            r.on('data', c => body += c);
-            r.on('end', () => {
-                var latestMeta = JSON.parse(body);
-                if(!latestMeta) return;
-                if(!latestMeta.version) return;
-                var current = this.parseVersion(this.meta.version), latest = this.parseVersion(latestMeta.version);
-                if (this.newerVersion(current, latest) == latest) this.log(`You are running an outdated version of DDBot. Current: ${this.meta.version} - Latest: ${latestMeta.version}`); else {
-                    this.log(`This is a development version of DDBot. Debug mode has been enabled.`);
-                    this.config.isDebugMode = true;
-                }
-            })
-        });
         process.on('unhandledRejection', rejection => {
             this.log(rejection, true);
         })
@@ -107,6 +90,9 @@ const client = new class extends Discord.Client {
         this.input = new InputUtilities(this);
         this.commandsManager = new CommandsManager(this);
         this.directMessagesManager = new DirectMessagesManager(this);
+        
+        this.config = require("./config");
+        this.meta = require("../package");
 
         mongoose.connect(this.config.database);
         autoIncrement.initialize(mongoose.connection);
@@ -500,38 +486,6 @@ const client = new class extends Discord.Client {
             })
         })
         this.login(this.config.token);
-    }
-
-    parseVersion(version) {
-        var versioning = {
-            major: null,
-            minor: null,
-            bugfix: null
-        }
-        version = version.split(".");
-        versioning.major = version[0] ? isNaN(version[0]) ? null : version[0] : null;
-        versioning.minor = version[1] ? isNaN(version[1]) ? null : version[1] : null;
-        versioning.bugfix = version[2] ? isNaN(version[2]) ? null : version[2] : null;
-        return versioning;
-    }
-
-    newerVersion(version, version1) {
-        if (version.major) {
-            if (version1.major) {
-                if (version.major > version1.major) return version; else if (version1.major > version.major) return version1;
-            } else return version;
-        } else if (version1.major) return version1;
-        if (version.minor) {
-            if (version1.minor) {
-                if (version.minor > version1.minor) return version; else if (version1.minor > version.minor) return version1;
-            } else return version;
-        }
-        if (version.bugfix) {
-            if (version1.bugfix) {
-                if (version.bugfix > version1.bugfix) return version; else if (version1.bugfix > version.bugfix) return version1;
-            } else return version;
-        }
-        return null;
     }
 
     configureForGuild(guild) {
