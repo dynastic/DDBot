49a5c91e7fcbd15737d7addd5a5b53577f397c38
betas <applebetas@icloud.com>
2017-08-20 18:45:25 -0400

    Only show admin eval shortcut if admin

--- Source/Modules/EvalShortcut/evalshortcut.js
+++ Source/Modules/EvalShortcut/evalshortcut.js
@@ -1,21 +1,21 @@
 class EvalShortcut {
   constructor (client) {
-    this.client = client
-    this.response = this.client.resolve('response')
-    if (!this.response) return this.client.log('[EvalShortcut] Disabling because response class could not be found', true)
-    this.response = require(this.response)
-    this.meta = require('./module')
-    this.config = require('./config')
+    this.client = client;
+    this.response = this.client.resolve('response');
+    if (!this.response) return this.client.log('[EvalShortcut] Disabling because response class could not be found', true);
+    this.response = require(this.response);
+    this.meta = require('./module');
+    this.config = require('./config');
     this.events = {
       message: (message) => {
-        if (message.content === this.config.prefix || !message.content.startsWith(this.config.prefix) || message.content.startsWith(this.config.prefix + this.config.prefix)) return
+        if (message.content === this.config.prefix || !message.content.startsWith(this.config.prefix) || message.content.startsWith(this.config.prefix + this.config.prefix) || !this.client.config.admins.includes(message.member.id)) return;
         this.client.commandsManager.get('eval').then(c => {
-          if (!c) return
-          var args = message.content.split(' ')
-          args[0] = args[0].slice(this.config.prefix.length)
-          args.unshift(`${client.config.prefix}eval`)
-          message.content = args.join(' ')
-          message.channel.type === 'dm' ? client.directMessagesManager.handle(message) : message.guild.manager.messagesManager.handle(message)
+          if (!c) return;
+          var args = message.content.split(' ');
+          args[0] = args[0].slice(this.config.prefix.length);
+          args.unshift(`${client.config.prefix}eval`);
+          message.content = args.join(' ');
+          message.channel.type === 'dm' ? client.directMessagesManager.handle(message) : message.guild.manager.messagesManager.handle(message);
         })
       }
     }
